1c55536dfbc77e7a7290b5c2dd57c4efc72f81cc
Flávio Fonsêca <77510058+flaviofonsecadev@users.noreply.github.com>
2023-04-05 23:26:59 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,4 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('meditacao-biblica').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/script.js',
-        '/style.css',
-      ]);
-    })
-  );
+self.addEventListener('fetch', function(event) {
+  console.log('fetch event:', event);
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
